e03de91d7b033ffe7150016fa60a7e42d7860c42
subhashishjungshah <subhashishjungshah@gmail.com>
2023-11-10 10:08:54 +0545

    playing around with react csv

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,47 @@
 import { CSVLink } from "react-csv";
 import "./App.css";
 import { useState } from "react";
-
-const csvData = [
-  ["firstname", "lastname", "email"],
-  ["Ahmed", "Tomi", "ah@smthing.co.com"],
-  ["Raed", "Labes", "rl@smthing.co.com"],
-  ["Yezzi", "Min l3b", "ymin@cocococo.com"],
-];
-const data = [
-  { something: "Ahmed", lastname: "Tomi", email: "ah@smthing.co.com" },
-  { something: "Raed", lastname: "Labes", email: "rl@smthing.co.com" },
-  { something: "Yezzi", lastname: "Min l3b", email: "ymin@cocococo.com" },
-];
-
-const response = [
-  {
-    "MEF OIT": "Yes",
-    "LSO APIs": "Product Catalog",
-    "LSO Cert": "Yes",
-    "LSO Releases": "Fergie",
-    selected_buy_sell: "Sell",
-    "LSO Product Payload": [
-      "Operator Ethernet",
-      "Subscriber Ethernet",
-      "Internet Access",
-      "Dark Fiber",
-      "Wavelength",
-      "5G FWA",
-      "Satellite",
-    ],
-    "UCs Product Catalog": [
-      "Item 2",
-      "Item 3",
-      "Item 4",
-      "Item 5",
-      "Item 6",
-      "Item 7",
-      "Item 10",
-      "Item 8",
-      "Item 9",
-    ],
-    "LSO Marketplace APIs": "Sell Product Catalog",
-    "Implementation status": "Development",
-  },
-];
+import axios from "axios";
 
 function App() {
   const [disabled, setDisabled] = useState(false);
+  const [data, setData] = useState([{}]);
+  const [loading, setLoading] = useState(false);
 
-  const handleClick = () => {
-    setDisabled(true);
-    setTimeout(() => {
-      setDisabled(false);
-    }, 5000);
+  const fetchData = async (event, done) => {
+    if (!loading) {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          "https://moviesdatabase.p.rapidapi.com/titles/x/upcoming",
+          {
+            headers: {
+              "X-RapidAPI-Key": process.env.X_RapidAPI_Key,
+              "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+            },
+          }
+        );
+        setData(response.data.results);
+        setLoading(false);
+        done(true);
+      } catch (error) {
+        console.error(error);
+        done(false);
+      }
+    }
+  };
+  const dataFromListOfUsersState = () => {
+    return data;
   };
 
   return (
     <>
-      <CSVLink data={response} filename="my_file.csv" onClick={handleClick}>
-        <button disabled={disabled}>Download me</button>
+      <CSVLink
+        data={dataFromListOfUsersState()}
+        asyncOnClick={true}
+        onClick={fetchData}
+      >
+        {loading ? "Loading csv..." : "Download me"}
       </CSVLink>
     </>
   );
---
+++ src/data/index.js
@@ -0,0 +1,43 @@
+export const csvData = [
+  ["firstname", "lastname", "email"],
+  ["Ahmed", "Tomi", "ah@smthing.co.com"],
+  ["Raed", "Labes", "rl@smthing.co.com"],
+  ["Yezzi", "Min l3b", "ymin@cocococo.com"],
+];
+export const data = [
+  { something: "Ahmed", lastname: "Tomi", email: "ah@smthing.co.com" },
+  { something: "Raed", lastname: "Labes", email: "rl@smthing.co.com" },
+  { something: "Yezzi", lastname: "Min l3b", email: "ymin@cocococo.com" },
+];
+
+export const response = [
+  {
+    "MEF OIT": "Yes",
+    "LSO APIs": "Product Catalog",
+    "LSO Cert": "Yes",
+    "LSO Releases": "Fergie",
+    selected_buy_sell: "Sell",
+    "LSO Product Payload": [
+      "Operator Ethernet",
+      "Subscriber Ethernet",
+      "Internet Access",
+      "Dark Fiber",
+      "Wavelength",
+      "5G FWA",
+      "Satellite",
+    ],
+    "UCs Product Catalog": [
+      "Item 2",
+      "Item 3",
+      "Item 4",
+      "Item 5",
+      "Item 6",
+      "Item 7",
+      "Item 10",
+      "Item 8",
+      "Item 9",
+    ],
+    "LSO Marketplace APIs": "Sell Product Catalog",
+    "Implementation status": "Development",
+  },
+];
